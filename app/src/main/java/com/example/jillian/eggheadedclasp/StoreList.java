9817d25bd11aad4de3665676b2201e9c1dea21d1
sade2 <cedricmcg2@gmail.com>
2018-10-27 22:06:21 -0400

    maybe fix

--- app/src/main/java/com/example/jillian/eggheadedclasp/StoreList.java
+++ app/src/main/java/com/example/jillian/eggheadedclasp/StoreList.java
@@ -10,18 +10,10 @@ import java.util.Set;
 
 public class StoreList{
     private final String IDENTIFIER = "eggheadClasp";
-    //if first time run, create new file
-    public StoreList() {
+    private ArrayList<ArrayList<String>> namesList = new ArrayList<>();
 
-    }
-
-    /**
-     * Checks through all bluetooth devices to find all eggheadClasp devices
-     * @param mBluetoothAdapter the bluetooth adapter to feed in to this method
-     */
-    public ArrayList<ArrayList<String>> findEggClasps(BluetoothAdapter mBluetoothAdapter){
+    public StoreList(BluetoothAdapter mBluetoothAdapter) {
         Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-        ArrayList<ArrayList<String>> namesList = new ArrayList<>();
         if (pairedDevices.size() > 0) {
             boolean aFoundDevice = false;
             // There are paired devices. Get the name and address of each paired device.
@@ -45,12 +37,59 @@ public class StoreList{
 
                 }
             }
+
+            //What if the user has never paired a egghead device before?
             if(!aFoundDevice){
                 Log.w("WARN: ", "No bluetooth connections found");
             }
         }
+    }
 
+    /**
+     * Adds an additional connection to the arraylist in the case that a new one is identified
+     * @param name The name of the bluetooth device
+     * @param userChangedName The specific user name of the device (if n/a, just use regular name)
+     * @param mac The mac address
+     */
+    public void addConnection(String name, String userChangedName, String mac){
+        ArrayList<String> toAdd = new ArrayList<>();
+        toAdd.add(name);
+        toAdd.add(userChangedName);
+        toAdd.add(mac);
+
+        namesList.add(toAdd);
+    }
+
+    /**
+     * Gets the number of connections
+     * @return how many total connections there are
+     */
+    public int numOfConnections(){
+        return namesList.size();
+    }
+
+    /**
+     * Gets the full list of lists of bluetooth connections
+     * @return The full nameList array
+     */
+    public ArrayList<ArrayList<String>> getNamesList(){
         return namesList;
+    }
+
+    /**
+     * Returns a list of specific parameters of an array list, i.e. a list of all mac addresses
+     * @param category Which category to choose (1 = name, 2 = user defined name, 3 = mac address)
+     * @return An array list of the selected category
+     */
+    public ArrayList<String> getCategoryList(int category){
+        ArrayList<String> categoryList = new ArrayList<>();
+        if(category<4){
+
+            for(int i = 0; i < namesList.size(); i++){
+                categoryList.add(namesList.get(i).get(category));
+            }
+        }
+        return categoryList;
     }
 
 }
