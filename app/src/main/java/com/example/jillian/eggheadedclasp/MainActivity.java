7c36280e7b8290f333e3a7f97e9a05286b68e753
sade2 <cedricmcg2@gmail.com>
2018-10-28 00:36:28 -0400

    jillian and andrew's dropdown bar code added

--- app/src/main/java/com/example/jillian/eggheadedclasp/MainActivity.java
+++ app/src/main/java/com/example/jillian/eggheadedclasp/MainActivity.java
@@ -1,31 +1,48 @@
 package com.example.jillian.eggheadedclasp;
 
+
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-//import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-import java.util.Set;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+//        parent = (LinearLayout) findViewById(R.id.ll_parentLayout);
+//
+//        for (int i = 0; i < 5; i++){
+//            Button b1 = new Button(MainActivity.this);
+//            b1.setId(i+1);
+//            b1.setText("egg");
+//            b1.setTag(i);
+//            parent.addView(b1);
+//            b1.setOnClickListener(MainActivity.this);
+//        }
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        String[] arrSpinner = new String[]{"1","2","3","4","5"};
+        Spinner s = (Spinner) findViewById(R.id.Bspinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, arrSpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -33,10 +50,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "hi", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+//                setContentView(R.layout.activity_main);
+//                parent = (LinearLayout) findViewById(R.id.ll_parentLayout);
+
+
             }
+
+
         });
-
-
     }
 
     @Override
@@ -104,13 +126,81 @@ public class MainActivity extends AppCompatActivity {
             onActivityResult(32, 3, enableIntent);
         }
 
-        Set<BluetoothDevice> pairedDevices = reader.getBondedDevices();
+        // Set<BluetoothDevice> pairedDevices = reader.getBondedDevices();
 
 
 
     }
 
 
+    @Override
+    public void onClick(View v) {
+
+//        Button b1 = new Button(MainActivity.this);
+//        int i = 4;
+//        b1.setId(R.id.b1);
+//        b1.setText("egg");
+//        b1.setTag(2);
+//        parent.addView(b1);
+//        b1.setOnClickListener(MainActivity.this);
+//
+//        String str = v.getTag().toString();
+//        if (str.equals("0")){
+//            Toast.makeText(getApplicationContext(),"click button 1", Toast.LENGTH_SHORT).show();
+//        }
+//        else if (str.equals("1")){
+//            Toast.makeText(getApplicationContext(),"click button 2", Toast.LENGTH_SHORT).show();
+//        }
+//        else if (str.equals("2")){
+//            Toast.makeText(getApplicationContext(),"click button 3", Toast.LENGTH_SHORT).show();
+//        }
+//        else if (str.equals("3")){
+//            Toast.makeText(getApplicationContext(),"click button 4", Toast.LENGTH_SHORT).show();
+//        }
+//        else {
+//            Toast.makeText(getApplicationContext(),"click button 5", Toast.LENGTH_SHORT).show();
+//        }
+    }
 
 
+
+    //      public void onClickConnect(View view) {
+//                //get the spinner from the xml.
+//                Spinner dropdown = findViewById(R.id.Bspinner);
+//
+//                // Create an ArrayAdapter using the string array and a default spinner layout
+//                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+//                        R.array.planets_array, android.R.layout.simple_spinner_item);
+//                // Specify the layout to use when the list of choices appears
+//                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//                // Apply the adapter to the spinner
+//                spinner.setAdapter(adapter);
+//                ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.ListView,StringArray);
+//                ListView listView = (ListView) findViewById(R.id.listview);
+//                listView.setAdapter(adapter);
+//
+//                class ListDisplay extends Activity {
+//                    // Array of strings...
+//                    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
+//                            "WebOS","Ubuntu","Windows7","Max OS X"};
+//
+//                    @Override
+//                    protected void onCreate(Bundle savedInstanceState) {
+//                        super.onCreate(savedInstanceState);
+//                        setContentView(R.layout.activity_main);
+//
+//                        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+//                                R.layout.activity_listview, mobileArray);
+//
+//                        ListView listView = (ListView) findViewById(R.id.mobile_list);
+//                        listView.setAdapter(adapter);
+//                    }
+//                }
+
+//                setContentView(R.layout.activity_main);
+//                parent = (LinearLayout) findViewById(R.id.ll_parentLayout);
+
+
+    //      }
+    //});
 }
